66bb96bbcceece39716e19b1e427f5ae88ae8d70
igoroctaviano <igoroctaviano@gmail.com>
2017-10-03 20:33:31 -0300

    Fix mobile style

--- novo/src/js/slider.js
+++ novo/src/js/slider.js
@@ -1,65 +1,85 @@
-$('.slider').each(function() {
+$(".slider").each(function() {
   var $this = $(this);
-  var $group = $this.find('.slide-group');
-  var $slides = $this.find('.slide');
+  var $group = $this.find(".slide-group");
+  var $slides = $this.find(".slide");
   var bulletArray = [];
   var currentIndex = 0;
   var timeout;
-  
+
   function move(newIndex) {
     var animateLeft, slideLeft;
-    
+
     advance();
-    
-    if ($group.is(':animated') || currentIndex === newIndex) { return; }
-    
-    bulletArray[currentIndex].removeClass('active');
-    bulletArray[newIndex].addClass('active');
-    
-    if (newIndex > currentIndex) {
-      slideLeft = '100%';
-      animateLeft = '-100%';
-    } else {
-      slideLeft = '-100%';
-      animateLeft = '100%';
+
+    if ($group.is(":animated") || currentIndex === newIndex) {
+      return;
     }
-    
-    $slides.eq(newIndex).css({ display: 'block', left: slideLeft });
-    $group.animate({
-      left: animateLeft
-    }, function() {
-      $slides.eq(currentIndex).css({ display: 'none' });
-      $slides.eq(newIndex).css({ left: 0 });
-      $group.css({ left: 0 });
-      currentIndex = newIndex;
-    });
+
+    bulletArray[currentIndex].removeClass("active");
+    bulletArray[newIndex].addClass("active");
+
+    if (newIndex > currentIndex) {
+      slideLeft = "100%";
+      animateLeft = "-100%";
+    } else {
+      slideLeft = "-100%";
+      animateLeft = "100%";
+    }
+
+    $slides.eq(newIndex).css({ display: "block", left: slideLeft });
+    $group.animate(
+      {
+        left: animateLeft
+      },
+      function() {
+        $slides.eq(currentIndex).css({ display: "none" });
+        $slides.eq(newIndex).css({ left: 0 });
+        $group.css({ left: 0 });
+        currentIndex = newIndex;
+      }
+    );
   }
-  
+
   function advance() {
     clearTimeout(timeout);
     timeout = setTimeout(function() {
-      if (currentIndex < ($slides.length - 1)) { move(currentIndex + 1); } 
-      else { move(0); }
+      if (currentIndex < $slides.length - 1) {
+        move(currentIndex + 1);
+      } else {
+        move(0);
+      }
     }, 4000);
   }
-  
-  $('.next-btn').on('click', function() {
-    if (currentIndex < ($slides.length - 1)) { move(currentIndex + 1); } 
-    else { move(0); }
+
+  $(".next-btn").on("click", function() {
+    if (currentIndex < $slides.length - 1) {
+      move(currentIndex + 1);
+    } else {
+      move(0);
+    }
   });
-  
-  $('.previous-btn').on('click', function() {
-    if (currentIndex !== 0) { move(currentIndex - 1); } 
-    else { move(3); }
+
+  $(".previous-btn").on("click", function() {
+    if (currentIndex !== 0) {
+      move(currentIndex - 1);
+    } else {
+      move(3);
+    }
   });
-  
+
   $.each($slides, function(index) {
     var $button = $('<a class="slide-btn">&bull;</a>');
-    
-    if (index === currentIndex) { $button.addClass('active'); }
-    $button.on('click', function() { move(index); }).appendTo('.slide-buttons');
+
+    if (index === currentIndex) {
+      $button.addClass("active");
+    }
+    $button
+      .on("click", function() {
+        move(index);
+      })
+      .appendTo(".slide-buttons");
     bulletArray.push($button);
   });
-  
+
   advance();
 });
