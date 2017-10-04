3ab0aab627be7f490ccc9b2ce4445e8edc699806
igoroctaviano <igoroctaviano@gmail.com>
2017-10-04 13:09:28 -0300

    Update splash

---
+++ src/js/smoothScroll.js
@@ -0,0 +1,22 @@
+$(function() {
+  var $window = $(window); // Window object
+
+  var scrollTime = 0.5; // Scroll time
+  var scrollDistance = 200; // Distance. Use smaller value for shorter scroll and greater value for longer scroll
+
+  $window.on("mousewheel DOMMouseScroll", function(event) {
+    event.preventDefault();
+
+    var delta =
+      event.originalEvent.wheelDelta / 120 || -event.originalEvent.detail / 3;
+    var scrollTop = $window.scrollTop();
+    var finalScroll = scrollTop - parseInt(delta * scrollDistance);
+
+    TweenMax.to($window, scrollTime, {
+      scrollTo: { y: finalScroll, autoKill: true },
+      ease: Power1.easeOut,
+      autoKill: true,
+      overwrite: 5
+    });
+  });
+});
